b17d9694b92657ea190d922c0d8e11010e35793d
sjpkg96 <70044158+sjpkg96@users.noreply.github.com>
2020-09-01 14:22:13 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -19,7 +19,22 @@ powerData <- powerData[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
 png("plot4.png", width=480, height=480)
 
 par(mfrow=c(2,2))
+# Plot 1
+plot(powerData[, dateTime], powerDT[, Global_active_power], type="l", xlab="", ylab="Global Active Power")
+
+# Plot 2
+plot(powerData[, dateTime],powerDT[, Voltage], type="l", xlab="datetime", ylab="Voltage")
+
+# Plot 3
+plot(powerData[, dateTime], powerDT[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
+lines(powerData[, dateTime], powerDT[, Sub_metering_2], col="red")
+lines(powerData[, dateTime], powerDT[, Sub_metering_3],col="blue")
+legend("topright", col=c("black","red","blue")
+       , c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  ")
+       , lty=c(1,1)
+       , bty="n"
+       , cex=.5) 
 # Plot 4
-plot(powerDT[, dateTime], powerDT[,Global_reactive_power], type="l", xlab="datetime", ylab="Global_reactive_power")
+plot(powerData[, dateTime], powerDT[,Global_reactive_power], type="l", xlab="datetime", ylab="Global_reactive_power")
 
 dev.off()
