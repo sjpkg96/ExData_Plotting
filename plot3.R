ba54d2ae467a03c13a87cf41eb57dfc486150da3
sjpkg96 <70044158+sjpkg96@users.noreply.github.com>
2020-09-01 14:20:44 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -27,12 +27,4 @@ legend("topright"
        ,lty=c(1,1), lwd=c(1,1))
 
 dev.off()
-# Plot 3
-plot(powerData[, dateTime], powerDT[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
-lines(powerData[, dateTime], powerDT[, Sub_metering_2], col="red")
-lines(powerData[, dateTime], powerDT[, Sub_metering_3],col="blue")
-legend("topright", col=c("black","red","blue")
-       , c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  ")
-       , lty=c(1,1)
-       , bty="n"
-       , cex=.5) 
+ 
